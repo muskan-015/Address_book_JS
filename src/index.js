425fce4d720ca166ee6e76d440a2454a7e7ea291
muskan-015 <muskanvds2004@gmail.com>
2025-03-03 13:47:00 +0530

    [Muskan] Add. UC3 functionality added

--- src/index.js
+++ src/index.js
@@ -7,15 +7,20 @@ try {
     const contact1 = new Contact(
         "Muskan", "Gupta", "Bhopal", "India", "M.P.", "101", "9876543210", "muskan@gmail.com"
     );
-    console.log(addressBook.addContact(contact1));
-    console.log(addressBook.getAllContacts());
+    console.log(addressBook.addContact(contact1)); 
+
+    const contact2 = new Contact(
+        "Khushi", "Gupta", "Bhopal", "India","M.P.","100","9197979988","khushi@gmail.com"
+    )
+    console.log(addressBook.addContact(contact2)); 
+    console.log("All Contacts", addressBook.getAllContacts());
 } catch (error) {
     console.error("Error:", error.message);
 }
 
 try {
     const invalidContact = new Contact(
-        "abc", "def", "ghi", "In", "mp", "1001", "12345", "muskan-email"
+        "ab", "cd", "abc", "In", "mp", "1001", "12345", "muskan-email"
     );
     console.log(addressBook.addContact(invalidContact));
 } catch (error) {
