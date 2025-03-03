6442f8481e621c48c6e6ea931163e5995c3633b3
muskan-015 <muskanvds2004@gmail.com>
2025-03-03 13:52:36 +0530

    [Muskan] Add. UC4 functionality added

--- src/index.js
+++ src/index.js
@@ -4,25 +4,30 @@ const Contact = require('./model/Contact');
 const addressBook = new AddressBook();
 
 try {
+   
     const contact1 = new Contact(
-        "Muskan", "Gupta", "Bhopal", "India", "M.P.", "101", "9876543210", "muskan@gmail.com"
+    "Muskan", "Gupta", "Bhopal", "India", "M.P.", "101", "9876543210", "muskan@gmail.com"
     );
     console.log(addressBook.addContact(contact1)); 
 
-    const contact2 = new Contact(
-        "Khushi", "Gupta", "Bhopal", "India","M.P.","100","9197979988","khushi@gmail.com"
-    )
+    
+const contact2 = new Contact(
+    "Khushi", "Gupta", "Bhopal", "India", "M.P.", "100", "9876541110", "khushi@gmail.com"
+);
     console.log(addressBook.addContact(contact2)); 
-    console.log("All Contacts", addressBook.getAllContacts());
+    console.log("All Contacts Before editing", addressBook.getAllContacts());
+    console.log(addressBook.editContact("Rakesh", { address: "Pipariya", city: "New York", phone: "9234567890" }));
+    console.log("All Contacts After editing", addressBook.getAllContacts());
 } catch (error) {
     console.error("Error:", error.message);
 }
 
 try {
     const invalidContact = new Contact(
-        "ab", "cd", "abc", "In", "mp", "1001", "12345", "muskan-email"
+        "ab", "cde", "abc", "In", "mp", "1001", "12345", "muskan-email"
     );
     console.log(addressBook.addContact(invalidContact));
 } catch (error) {
     console.error("Error:", error.message);
 }
+
